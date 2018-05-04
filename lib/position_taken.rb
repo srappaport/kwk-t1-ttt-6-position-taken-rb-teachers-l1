a5a37e60eba0a7d03e3bba4b5ffd32b6d6345ced
srappaport <sarah.rappaport@greendot.org>
2018-05-04 20:25:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,9 @@
 def position_taken?(array, index)
   if array[index] == " "
     false
-  else array[index] == ""
+  elsif array[index] == ""
     false
+  else array[index]== "X" || array[index] == "O"
+    true
   end
 end
