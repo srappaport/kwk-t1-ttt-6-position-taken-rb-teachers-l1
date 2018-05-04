3af52c8e3b843f753740ca13cf5f0a58f69ffbd0
srappaport <sarah.rappaport@greendot.org>
2018-05-04 20:27:23 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here
+def position_taken?(array, index)
+  if array[index] == " "
+    false
+  elsif array[index] == "" || array[index]== nil
+    false
+  else array[index]== "X" || array[index] == "O"
+    true
+  end
+end
